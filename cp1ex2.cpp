d1789e0c1a7fabc65e2ce1db325415f1e1452c58
opkEh <56930383+opkpEh@users.noreply.github.com>
2022-11-07 07:41:49 +0530

    Restarting my cpp journey

---
+++ cp1ex2.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+int main(){
+      std::cout << "Enter the two numbers to be mulitplied: " << std::endl;
+      int var1= 0, var2=0;
+      std::cin >> var1 >> var2 ;
+      std::cout << "The multipliction of " << var1 << " and " << var2 << " is " << var1 *var2 << std::endl;
+}
---
+++ cp1ex3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(){
+      int val= 50, sum=0;
+      while (50<= val){
+            sum += val +1;
+            ++val;
+      }
+
+      std::cout<< "The inclisive vale from 50 to 100 is " << sum << std::endl;
+}
